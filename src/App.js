63e08a6ffba633e6893620c494dd8093f5193479
Wendellsg <wendell.silva.guimaraes@gmail.com>
2021-01-28 20:07:52 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
+import logo from './Assets/Logovideolab.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      
+
+        <img src={logo} className="Logo-VLF" alt="Logo VIDEOLAB"/>
+        <p className="Sombras">
+          BEM-VINDO AO SITE DA VIDEOLAB FILMES
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/videolabfilmes"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Nosso Instagram
         </a>
+
       </header>
     </div>
   );
