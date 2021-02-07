7cc77c30a36c22beec3a2f79701c0514d0c8b37e
Wendellsg <wendell.silva.guimaraes@gmail.com>
2021-02-07 17:12:45 -0300

    Correção do overflow

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -5,34 +5,31 @@ import {useState} from 'react'
 function AppHeader(){
 
     const [show, setShow] = useState(true);
-    
+    const BodyWidth = window.innerWidth
 
    function handleClick(){
         setShow(!show)
-        document.body.style.overflow = show? "hidden": "initial"
-        
-        
+        if(BodyWidth < 430)
+            
+            document.body.style.overflow = show?"hidden":"initial"
+   }; 
 
-        
-    };
-
-    
 
     return(
         <header className="Header-container">
             <div className="Applogo">
                 <a href="https://videolabfilmes.com.br/"><img src={logo} alt="VideoLab Filmes"/></a>
             </div>
-            <div className={`App-nav ${show? '':'on' }`} onClick={handleClick}>
+            <div className={`App-nav ${show? '':'on' }`} onClick={handleClick}> 
 
-                <div className="menu-toggle" onClick={handleClick}>
+                <div className="menu-toggle" >
                     <div className="one"></div>
                     <div className="two"></div>
                     <div className="three"></div>
                 </div>
 
 
-                <nav>
+                <nav >
                     <li>
                         <ul><a href="#Sobre-nos">Sobre nós</a></ul>
                         <ul><a href="#Portifólio">Portifólio</a></ul>
