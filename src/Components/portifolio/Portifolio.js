7f25aaca9a706ef0d63ec2161cd97a9ac432f127
Wendellsg <wendell.silva.guimaraes@gmail.com>
2021-02-07 19:26:13 -0300

    Title no iframe

--- src/Components/portifolio/Portifolio.js
+++ src/Components/portifolio/Portifolio.js
@@ -5,12 +5,12 @@ function Portifolio(){
         <div id="Portifólio" className="portifolio-container">
             <h1>Portifólio</h1>
             <div className="videos-cards">
-            <iframe className="card" src="https://www.youtube.com/embed/vLE8o0povRo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <iframe className="card" src="https://www.youtube.com/embed/vTK6tIk8U9o" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <iframe className="card" height="315" src="https://www.youtube.com/embed/dzPe1RhNd04" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <iframe  className="card" src="https://www.youtube.com/embed/OfR7z3WersI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <iframe  className="card" src="https://www.youtube.com/embed/-vpgHkm1mbQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <iframe className="card" src="https://www.youtube.com/embed/DpXnwRTw5pI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe title="beat" className="card" src="https://www.youtube.com/embed/vLE8o0povRo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe  title="zapguia"className="card" src="https://www.youtube.com/embed/vTK6tIk8U9o" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe title="sejaleve"className="card" height="315" src="https://www.youtube.com/embed/dzPe1RhNd04" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe  title="gaya" className="card" src="https://www.youtube.com/embed/OfR7z3WersI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe  title="timelife" className="card" src="https://www.youtube.com/embed/-vpgHkm1mbQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe title="Megazap" className="card" src="https://www.youtube.com/embed/DpXnwRTw5pI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
             <a href="https://www.youtube.com/channel/UCNObqaOyPSktpxJLcJOh2TQ" ><button className="portifolio-btn">VER TUDO</button></a>
         </div>
