bf5b3eb42dec4a5a080e8a4b94f9cfbaf03409d6
Wendellsg <wendell.silva.guimaraes@gmail.com>
2021-02-08 11:25:04 -0300

    Finalizada a aba de contato

--- src/Components/Contato/Contato.js
+++ src/Components/Contato/Contato.js
@@ -1,20 +1,33 @@
 import './styles.css'
+import {useState,useEffect} from 'react'
 
 
 
 function Contato() {
 
+    const [input, setInput] = useState('')
+    const [msg, setMsg] = useState('')
+
+        useEffect(() => {
+         let encoded = encodeURI(input)
+
+        setMsg(encoded)
+        }, [input]);
+      
+
+  
     return(
         <div className="contato-container">
             <h1>Fale conosco</h1>
             <div className="contact-card">            
                 <h2>Mande uma mensagem via Whatsapp</h2>
                 <div className="custom-msg">
-                <input  className="text-area" placeholder="" type="text" name="mensagem" id="mensagem"/>
-               <button className="wpp-btn" >Enviar Mensagem</button>
-                </div>
-                         
-            </div>  
+                    <input  value={input} onInput={e => setInput(e.target.value)} className="text-area" placeholder="" type="text" name="mensagem" id="mensagem"/>
+                    <a href={`https://api.whatsapp.com/send?phone=5511981050758&text=${msg}`}><button className="wpp-btn">Enviar Mensagem</button></a>
+                </div> 
+           
+            </div> 
+            
         </div>
     )
 }
--- src/Components/footer/footer.js
+++ src/Components/footer/footer.js
@@ -16,7 +16,7 @@ function appFooter(){
             <div className="social-networks">
             <h2>CONTATE-NOS VIA</h2>
             <div className="sn-logos">
-                <a href="https://api.whatsapp.com/send?phone=5511981050758&text=Ol%C3%A1%2C%20conheci%20voc%C3%AAs%20pelo%20site!"><img src={Whatsapp} alt=""/></a>
+                <a href="https://api.whatsapp.com/send?phone=5511981050758&text=Ol%C3%A1%2C%20conheci%20voc%C3%AAs%20pelo%20site!https://api.whatsapp.com/send?phone=5511981050758&text=Ol%C3%A1%2C%20conheci%20voc%C3%AAs%20pelo%20site!"><img src={Whatsapp} alt=""/></a>
                 <a href="https://www.instagram.com/videolabfilmes/"><img src={Instagram} alt=""/></a>
                 <a href="https://www.facebook.com/videolabfilmes"><img src={Facebook} alt=""/></a>
 
