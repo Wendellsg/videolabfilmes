c59db71e8212a1d3c4fe370411aa4564b84d37c8
Wendellsg <wendell.silva.guimaraes@gmail.com>
2021-02-08 17:36:29 -0300

    Trocado os vídeos

--- src/Components/portifolio/Portifolio.js
+++ src/Components/portifolio/Portifolio.js
@@ -5,10 +5,10 @@ function Portifolio(){
         <div className="portifolio-container">
             <h1>Portifólio</h1>
             <div className="videos-cards">
-            <iframe title="beat" className="card" src="https://www.youtube.com/embed/vLE8o0povRo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe title="Levis" className="card" src="https://www.youtube.com/embed/VTyrXhTbm1M" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <iframe  title="zapguia"className="card" src="https://www.youtube.com/embed/vTK6tIk8U9o" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <iframe title="sejaleve"className="card" height="315" src="https://www.youtube.com/embed/dzPe1RhNd04" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <iframe  title="gaya" className="card" src="https://www.youtube.com/embed/OfR7z3WersI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe  title="cultura" className="card" src="https://www.youtube.com/embed/9_g5qBoMxus" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <iframe  title="timelife" className="card" src="https://www.youtube.com/embed/-vpgHkm1mbQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <iframe title="Megazap" className="card" src="https://www.youtube.com/embed/DpXnwRTw5pI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
